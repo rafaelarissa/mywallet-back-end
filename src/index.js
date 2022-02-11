2b58cf92267753f007aa2870203d29866b1c08e4
rafaelarissa <rafaela.larissalj@gmail.com>
2022-02-11 00:38:11 -0300

    build: added db & index file

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import express, {json} from 'express';
+import cors from 'cors';
+
+const server = express();
+server.use(cors());
+server.use(json());
+
+server.listen(5000, () => {
+  console.log("Listening on 5000")
+})
